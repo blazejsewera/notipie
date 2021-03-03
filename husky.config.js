a82697d1e0ae5c7df8e9c8a2121dd983fee47bf1
Blazej Sewera <code@sewera.dev>
2021-03-03 16:04:58 +0100

    feat: move eslint and prettier configs to root dir

--- husky.config.js
+++ husky.config.js
@@ -2,7 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': tasks(['cd notipie-ui', 'yarn lint', 'cd ..']),
+    'pre-commit': tasks(['cd notipie-ui', 'yarn lint', 'cd ..', 'cd notipie-ui-components', 'yarn lint', 'cd ..']),
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
