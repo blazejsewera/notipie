c4d857606fc41862c51de9037103cb9358bbe83d
Blazej Sewera <code@sewera.dev>
2021-10-16 19:06:31 +0200

    fix(ui): add intl object to stories

--- ui/story/notification/card/NotificationCard.tsx
+++ ui/story/notification/card/NotificationCard.tsx
@@ -3,6 +3,7 @@ import { Meta, Story } from '@storybook/react'
 import type { NotificationCardProps } from '../../../src/component/notification/card/NotificationCard'
 import { NotificationCard } from '../../../src/component/notification/card/NotificationCard'
 import { fiveSentenceLoremIpsum } from '../../mock/lipsum'
+import { intlMock } from '../../../src/mock/intl.mock'
 
 export default {
   title: 'notification/card/NotificationCard',
@@ -12,6 +13,7 @@ export default {
 export const ExampleNotificationCard: Story<NotificationCardProps> = (args) => <NotificationCard {...args} />
 
 ExampleNotificationCard.args = {
+  intl: intlMock,
   notification: {
     appName: 'Example App',
     timestamp: '2 hours ago',
--- ui/story/notification/container/NotificationContainer.tsx
+++ ui/story/notification/container/NotificationContainer.tsx
@@ -3,6 +3,7 @@ import { Meta, Story } from '@storybook/react'
 import type { NotificationContainerProps } from '../../../src/component/notification/container/NotificationContainer'
 import { NotificationContainer } from '../../../src/component/notification/container/NotificationContainer'
 import { fiveSentenceLoremIpsum } from '../../mock/lipsum'
+import { intlMock } from '../../../src/mock/intl.mock'
 
 export default {
   title: 'notification/container/NotificationContainer',
@@ -14,6 +15,7 @@ export const ExampleNotificationContainer: Story<NotificationContainerProps> = (
 )
 
 ExampleNotificationContainer.args = {
+  intl: intlMock,
   notifications: [
     { appName: 'A1', title: 'Title 1', timestamp: '2 hours ago', uuid: '1' },
     {
