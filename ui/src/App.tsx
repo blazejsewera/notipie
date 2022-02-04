61b3fa35773a311c8584010919e5c8fc35934b58
Blazej Sewera <code@sewera.dev>
2022-02-04 22:36:31 +0100

    fix(ui): update main app component's imports

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
 import { FC } from './type/react'
 import { intl } from './i18l/intl'
-import { cx } from './util/cx'
-import { full, fullWithImage, minimal, partial } from './mock/notification.mock'
+import { cx } from './util/classname/cx'
+import {
+  fullWithHandlers,
+  fullWithImageWithHandlers,
+  minimalWithHandlers,
+  partialWithHandlers,
+} from './mock/notification.mock'
 import { handlers as mockContainerHandlers } from './mock/notificationContainer.mock'
 import './style/main.css'
 import './style/inter.css'
@@ -10,13 +15,21 @@ import { NotificationContainer } from './component/notification/container/Notifi
 import { ToggleDarkModeButton } from './component/button/ToggleDarkModeButton'
 
 export const App: FC = () => {
-  const notifications = [full, fullWithImage, partial, minimal]
+  const notificationsWithHandlers = [
+    fullWithHandlers,
+    fullWithImageWithHandlers,
+    partialWithHandlers,
+    minimalWithHandlers,
+  ]
   const [isDarkMode, setDarkMode] = React.useState(false)
   const toggleDarkMode = () => setDarkMode(!isDarkMode)
   return (
     <div className={cx('App', 'min-h-screen', isDarkMode ? 'dark' : '')}>
       <div className={cx('bg-gray-200', 'dark:bg-gray-600', 'min-h-screen', 'p-10')}>
-        <NotificationContainer title="Tag title" {...{ notifications, handlers: mockContainerHandlers, intl }} />
+        <NotificationContainer
+          title="Tag title"
+          {...{ notificationsWithHandlers, handlers: mockContainerHandlers, intl }}
+        />
       </div>
       <div className={cx('fixed', 'top-5', 'right-5')}>
         <ToggleDarkModeButton onClick={toggleDarkMode} isDarkMode={isDarkMode} />
