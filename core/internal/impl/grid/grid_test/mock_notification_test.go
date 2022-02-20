870880bfb4da66834d71ecc502f8e5a26aff1fc9
Blazej Sewera <code@sewera.dev>
2022-02-20 20:14:18 +0100

    fix(core): fix mock notification creation

--- core/internal/impl/grid/grid_test/mock_notification_test.go
+++ core/internal/impl/grid/grid_test/mock_notification_test.go
@@ -4,15 +4,17 @@ import "github.com/jazzsewera/notipie/core/internal/impl/model"
 
 func NewTestNetNotification() model.NetNotification {
 	return model.NetNotification{
-		AppName:    "TestApp",
-		Timestamp:  "2022-02-19T11:00:00.000Z",
-		Title:      "Test NetNotification",
-		Subtitle:   "Test Subtitle",
-		Body:       "Test Body",
-		ID:         "1",
-		Read:       false,
-		ExtUri:     "testExtUri",
-		ReadUri:    "testReadUri",
-		ArchiveUri: "testArchiveUri",
+		HashableNetNotification: model.HashableNetNotification{
+			AppName:    "TestApp",
+			Title:      "Test NetNotification",
+			Subtitle:   "Test Subtitle",
+			Body:       "Test Body",
+			ExtUri:     "testExtUri",
+			ReadUri:    "testReadUri",
+			ArchiveUri: "testArchiveUri",
+		},
+		Timestamp: "2022-02-19T11:00:00.000Z",
+		ID:        "1",
+		Read:      false,
 	}
 }
