556bbb11844083b4ca47e52659eec0cb5463fe48
Blazej Sewera <code@sewera.dev>
2022-05-04 22:39:09 +0200

    chore(test): tweak notifications server params

--- test/notifications-server/notification.ts
+++ test/notifications-server/notification.ts
@@ -15,30 +15,41 @@ export type Notification = {
   relativeTime?: string
 }
 
-const generateArray = <T>(length: number, factory: () => T): T[] => Array.from([...Array(length)], factory)
-
-const optional = <T>(element: T) => (faker.datatype.boolean() ? element : undefined)
+namespace util {
+  export const generateArray = <T>(length: number, factory: () => T): T[] => Array.from([...Array(length)], factory)
+  export const randBool = faker.datatype.boolean
+  export const optional = <T>(element: T) => (randBool() ? element : undefined)
+  export const alternative = <T>(first: T, second: T) => (randBool() ? first : second)
+  export const titleCase = (s: string) => s.slice(0, 1).toUpperCase().concat(s.slice(1))
+}
 
 const exampleAppImgUri = 'https://fonts.gstatic.com/s/i/materialiconsround/star/v20/24px.svg'
 
 const mockNotification = (appName: string): Notification => ({
   appName,
   timestamp: faker.date.recent().toISOString(),
-  title: `${faker.word.verb()} ${faker.word.noun()}`,
-  subtitle: optional(`${faker.word.adverb()} ${faker.word.verb()} ${faker.word.adjective()} ${faker.word.noun()}`),
-  body: optional(faker.lorem.paragraph()),
+  title: util.alternative(
+    `${faker.word.verb()} ${faker.word.noun()}`,
+    `${faker.word.verb()} ${faker.word.adjective()} ${faker.word.noun()}`,
+  ),
+  subtitle: util.optional(`${faker.word.adverb()} ${faker.word.verb()} ${faker.word.adjective()} ${faker.word.noun()}`),
+  body: util.optional(faker.lorem.paragraph()),
   id: faker.random.alphaNumeric(45),
-  read: faker.datatype.boolean(),
-  appImgUri: optional(exampleAppImgUri),
+  read: util.randBool(),
+  appImgUri: util.optional(exampleAppImgUri),
   archiveUri: faker.internet.url(),
   readUri: faker.internet.url(),
   extUri: faker.internet.url(),
 })
 
-const mockAppName = (): string => faker.company.companyName()
+const mockAppName = (): string =>
+  util.alternative(
+    `${util.titleCase(faker.word.adjective())} ${util.titleCase(faker.word.noun())}`,
+    util.titleCase(faker.word.noun()),
+  )
 
 export const getMockNotifications = (length: number): Notification[] => {
   const mockAppNamesLength = length < 3 ? 1 : Math.floor(length / 3)
-  const mockAppNames = generateArray(mockAppNamesLength, mockAppName)
-  return generateArray(length, () => mockNotification(faker.helpers.arrayElement(mockAppNames)))
+  const mockAppNames = util.generateArray(mockAppNamesLength, mockAppName)
+  return util.generateArray(length, () => mockNotification(faker.helpers.arrayElement(mockAppNames)))
 }
