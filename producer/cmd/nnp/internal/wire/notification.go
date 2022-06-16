cb08215dbae004812ebee728bf7372333dad0ee3
Blazej Sewera <code@sewera.dev>
2022-06-16 23:11:20 +0200

    test(producer): add test links to example notification

--- producer/cmd/nnp/internal/wire/notification.go
+++ producer/cmd/nnp/internal/wire/notification.go
@@ -2,7 +2,18 @@ package wire
 
 import "github.com/blazejsewera/notipie/core/pkg/model"
 
-var exampleAppNotification = model.ExampleAppNotification
+var exampleAppNotification = model.AppNotification{
+	HashableNetNotification: model.HashableNetNotification{
+		AppName:    "Example App Name",
+		AppImgURI:  "https://www.sewera.dev/magpie_dark.svg",
+		Title:      "Example Notification",
+		Subtitle:   "Subtitle",
+		Body:       "Body",
+		ExtURI:     "https://www.sewera.dev/",
+		ReadURI:    "https://www.sewera.dev/",
+		ArchiveURI: "https://www.sewera.dev/",
+	},
+}
 
 func GetAppNotification() (model.AppNotification, error) {
 	return exampleAppNotification, nil
